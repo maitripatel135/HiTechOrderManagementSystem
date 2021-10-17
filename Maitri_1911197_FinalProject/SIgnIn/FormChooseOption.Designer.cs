d608aa6c33dc7c6ff1b8cf2d96589db4859a154d
maitripatel135 <maitri_hemangkumar.patel@lcieducation.net>
2021-10-16 21:21:36 -0400

    Add project files.

---
+++ Maitri_1911197_FinalProject/SIgnIn/FormChooseOption.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Final_Project_Part_1.GUI
+{
+    partial class FormChooseOption
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(257, 34);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Please Choose from below that In which\r\ntable you want to do modifications?\r\n";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(118, 69);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(148, 21);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "User / Employee";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(118, 95);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(97, 21);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Customer";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(118, 123);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(65, 21);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Book";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton4.Location = new System.Drawing.Point(118, 150);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(71, 21);
+            this.radioButton4.TabIndex = 8;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Order";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton5.Location = new System.Drawing.Point(118, 177);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(80, 21);
+            this.radioButton5.TabIndex = 9;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Invoice";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(108, 374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "LOG IN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(294, 374);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 31);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "EXIT";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(225, 235);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(88, 238);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "USER ID :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(88, 286);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 17);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "PASSWORD :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(225, 283);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 15;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // FormChooseOption
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 453);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label3);
+            this.Name = "FormChooseOption";
+            this.Text = "FormChooseOption";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ Maitri_1911197_FinalProject/SIgnIn/FormChooseOption.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Final_Project_Part_1.GUI;
+
+
+namespace Final_Project_Part_1.GUI
+{
+    public partial class FormChooseOption : Form
+    {
+        public FormChooseOption()
+        {
+            InitializeComponent();
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                if (textBox1.Text == " " || textBox2.Text == " ")
+                {
+                    MessageBox.Show("Please enter UserId and Passsword");
+                }
+                else
+                {
+                    string temp1 = textBox1.Text.Trim();
+                    string temp2 = textBox2.Text.Trim();
+                    if (!Validator.IsValidId(temp1, 4))
+                    {
+                        MessageBox.Show("Invalid User Id", "Error");
+                        textBox1.Clear();
+                        textBox1.Focus();
+                        return;
+                    }
+                    HiTech us = new HiTech();
+                    us = us.GetUser(Convert.ToInt32(textBox1.Text.Trim()));
+                    if (us != null)
+                    {
+                        if (textBox1.Text == us.UserId.ToString() && textBox2.Text != us.Password)
+                        {
+                            MessageBox.Show("Invalid Password!!!", "Warning");
+                            textBox2.Clear();
+                            textBox2.Focus();
+                            return;
+                        }
+                        else if (textBox1.Text == us.UserId.ToString() && textBox2.Text == us.Password && us.EmployeeId == 222)
+                        {
+                            FormHiTech fh = new FormHiTech();
+                            fh.ShowDialog();
+                        }
+                        else if (textBox1.Text == us.UserId.ToString() && textBox2.Text == us.Password && us.EmployeeId != 222)
+                        {
+                            MessageBox.Show("Sorry !! Only MIS Managers can modity tables User & Employee !! ", "Warning");
+                        }
+                    }
+                }
+            }
+            else if (radioButton2.Checked)
+            {
+                if (textBox1.Text == " " || textBox2.Text == " ")
+                {
+                    MessageBox.Show("Please enter UserId and Passsword");
+                }
+                else
+                {
+                    string temp1 = textBox1.Text.Trim();
+                    string temp2 = textBox2.Text.Trim();
+                    if (!Validator.IsValidId(temp1, 4))
+                    {
+                        MessageBox.Show("Invalid User Id", "Error");
+                        textBox1.Clear();
+                        textBox1.Focus();
+                        return;
+                    }
+                    HiTech us = new HiTech();
+                    us = us.GetUser(Convert.ToInt32(textBox1.Text.Trim()));
+                    if (us != null)
+                    {
+                        if (textBox1.Text == us.UserId.ToString() && textBox2.Text != us.Password)
+                        {
+                            MessageBox.Show("Invalid Password!!!", "Warning");
+                            textBox2.Clear();
+                            textBox2.Focus();
+                            return;
+                        }
+                        else if (textBox1.Text == us.UserId.ToString() && textBox2.Text == us.Password && us.JobId == 3333)
+                        {
+                            //FormCustomer fc = new FormCustomer();
+                            //fc.ShowDialog();
+                        }
+                        else if (textBox1.Text == us.UserId.ToString() && textBox2.Text == us.Password && us.JobId != 2222)
+                        {
+                            MessageBox.Show("Sorry !! Only Sales Managers can modity table Customer !! ", "Warning");
+                        }
+                    }
+                }
+            }
+            else if (radioButton3.Checked)
+            {
+                if (textBox1.Text == " " || textBox2.Text == " ")
+                {
+                    MessageBox.Show("Please enter UserId and Passsword");
+                }
+                else
+                {
+                    string temp1 = textBox1.Text.Trim();
+                    string temp2 = textBox2.Text.Trim();
+                    if (!Validator.IsValidId(temp1, 4))
+                    {
+                        MessageBox.Show("Invalid User Id", "Error");
+                        textBox1.Clear();
+                        textBox1.Focus();
+                        return;
+                    }
+                    HiTech us = new HiTech();
+                    us = us.GetUser(Convert.ToInt32(textBox1.Text.Trim()));
+                    if (us != null)
+                    {
+                        if (textBox1.Text == us.UserId.ToString() && textBox2.Text != us.Password)
+                        {
+                            MessageBox.Show("Invalid Password!!!", "Warning");
+                            textBox2.Clear();
+                            textBox2.Focus();
+                            return;
+                        }
+                        else if (textBox1.Text == us.UserId.ToString() && textBox2.Text == us.Password && us.JobId == 3333)
+                        {
+                            //FormCustomer fc = new FormCustomer();
+                            //fc.ShowDialog();
+                        }
+                        else if (textBox1.Text == us.UserId.ToString() && textBox2.Text == us.Password && us.JobId != 2222)
+                        {
+                            MessageBox.Show("Sorry !! Only Inventory Controllers can modity table Book !! ", "Warning");
+                        }
+                    }
+                }
+            }
+            else if (radioButton4.Checked)
+            {
+                if (textBox1.Text == " " || textBox2.Text == " ")
+                {
+                    MessageBox.Show("Please enter UserId and Passsword");
+                }
+                else
+                {
+                    string temp1 = textBox1.Text.Trim();
+                    string temp2 = textBox2.Text.Trim();
+                    if (!Validator.IsValidId(temp1, 4))
+                    {
+                        MessageBox.Show("Invalid User Id", "Error");
+                        textBox1.Clear();
+                        textBox1.Focus();
+                        return;
+                    }
+                    HiTech us = new HiTech();
+                    us = us.GetUser(Convert.ToInt32(textBox1.Text.Trim()));
+                    if (us != null)
+                    {
+                        if (textBox1.Text == us.UserId.ToString() && textBox2.Text != us.Password)
+                        {
+                            MessageBox.Show("Invalid Password!!!", "Warning");
+                            textBox2.Clear();
+                            textBox2.Focus();
+                            return;
+                        }
+                        else if (textBox1.Text == us.UserId.ToString() && textBox2.Text == us.Password && us.JobId == 3333)
+                        {
+                            //FormCustomer fc = new FormCustomer();
+                            //fc.ShowDialog();
+                        }
+                        else if (textBox1.Text == us.UserId.ToString() && textBox2.Text == us.Password && us.JobId != 2222)
+                        {
+                            MessageBox.Show("Sorry !! Only Order Clerks can modity table Order !! ", "Warning");
+                        }
+                    }
+                }
+            }
+            else if (radioButton5.Checked)
+            {
+                if (textBox1.Text == " " || textBox2.Text == " ")
+                {
+                    MessageBox.Show("Please enter UserId and Passsword");
+                }
+                else
+                {
+                    string temp1 = textBox1.Text.Trim();
+                    string temp2 = textBox2.Text.Trim();
+                    if (!Validator.IsValidId(temp1, 4))
+                    {
+                        MessageBox.Show("Invalid User Id", "Error");
+                        textBox1.Clear();
+                        textBox1.Focus();
+                        return;
+                    }
+                    HiTech us = new HiTech();
+                    us = us.GetUser(Convert.ToInt32(textBox1.Text.Trim()));
+                    if (us != null)
+                    {
+                        if (textBox1.Text == us.UserId.ToString() && textBox2.Text != us.Password)
+                        {
+                            MessageBox.Show("Invalid Password!!!", "Warning");
+                            textBox2.Clear();
+                            textBox2.Focus();
+                            return;
+                        }
+                        else if (textBox1.Text == us.UserId.ToString() && textBox2.Text == us.Password && us.JobId == 3333)
+                        {
+                            //FormCustomer fc = new FormCustomer();
+                            //fc.ShowDialog();
+                        }
+                        else if (textBox1.Text == us.UserId.ToString() && textBox2.Text == us.Password && us.JobId != 2222)
+                        {
+                            MessageBox.Show("Sorry !! Only Accountant can modity table Invoices !! ", "Warning");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+        
+    
+}
